7aa703903ba0a25b7f6334a304a4d6e40d88d302
Jeremy <encaladajeremy@gmail.com>
2025-01-22 23:07:26 -0500

    Update NPC instructions to include response handling for non-Spanish questions

--- index.js
+++ index.js
@@ -7,21 +7,21 @@ dotenv.config()
 const apiKey = process.env.GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
-const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-flash",
-    systemInstruction: instructions
-});
-
-const generationConfig = {
-    temperature: 1,
-    topP: 0.95,
-    topK: 40,
-    maxOutputTokens: 8192,
-    responseMimeType: "application/json",
-};
-
-
 export async function run(text) {
+    
+    const model = genAI.getGenerativeModel({
+        model: "gemini-1.5-flash",
+        systemInstruction: instructions
+    });
+    
+    const generationConfig = {
+        temperature: 1,
+        topP: 0.95,
+        topK: 40,
+        maxOutputTokens: 8192,
+        responseMimeType: "application/json",
+    };
+
     try{
         console.log(text);
         const chatSession = model.startChat({
